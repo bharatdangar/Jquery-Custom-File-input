1c5f8ac19b75d8bac89c2a2fc799b13ba4387321
bharatdangar <bharatdangar@gmail.com>
2015-06-05 18:25:55 +0530

    Update customfileinput.js

--- js/customfileinput.js
+++ js/customfileinput.js
@@ -16,7 +16,7 @@
 			//Set Default setting for plugin
 			var defaults = {
                 buttontext : 'Send',
-				customboxwidth : 400,
+				customboxwidth : '98%',
 				customboxclass : 'customfile',
 				fileinputclass : 'fileinput'
             };
@@ -24,12 +24,12 @@
 			//Define Default option using extend 
             var options = $.extend(defaults, options);
 			
-			var fileinput = $(this).find('input[type=file]');
+			var fileinput = $(this).find('input[type=file]').css({"width":"100%", "height":"100%", "position":"absolute", "left":"0", "top":"0" });
 			fileinput.addClass(options.fileinputclass); //adding Class in to File input
 			fileinput.css({opacity:0}); // Hide File input
 			
 			//create Element for custom design
-			var customfile = $('<div class="'+options.customboxclass+'"></div>'); //custome file wrapper
+			var customfile = $('<div class="'+options.customboxclass+'"><div class="innersec"></div></div>').css({"position":"relative"}); //custome file wrapper
 			var cfilefield = $('<span class="cust-field"></span>'); //custome file field
 			var cfilebutton = $('<span class="cust-btn"></span>').text(options.buttontext); //custome file button
 			
@@ -51,10 +51,11 @@
 				var offtop = $(this).offset().top;
 				var leftpos =  e.pageX - offleft - 190;
 				var toppos =  e.pageY - offtop-5 ;
-				$(this).find('input[type=file]').css({left: leftpos, top: toppos});
-			}).bind('mouseout', function(e){
+			});
+			$("."+movebox).bind('mouseleave', function(e){
 				$(this).find('input[type=file]').css({left: 0, top: 0});
 			}).width(options.customboxwidth);
+			
 		}
 	});
 /*----------------------------------------------------------------------------------------------------*/	
