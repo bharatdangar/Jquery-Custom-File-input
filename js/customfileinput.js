d7ba150a5c83df5f59273c83e65e8d1b6a07acb9
bharatdangar <bharatdangar@gmail.com>
2016-06-10 15:29:03 +0530

    Update customfileinput.js

--- js/customfileinput.js
+++ js/customfileinput.js
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------//
-// 	CustomFile Input - jQUERY Plugin for Custome File design 	//
+// CustomFile Input - jQUERY Plugin for Custome File design 	//
 //                                                	 			//
 //  Author: Bharat Dangar (bharatdangar@gmail.com) 				//
 //  Version: 1.0                                   				//	
@@ -12,7 +12,6 @@
 	$.fn.extend({
 		//Plugin Code Start here
 		customFileinput: function(options){
-			
 			//Set Default setting for plugin
 			var defaults = {
                 buttontext : 'Send',
@@ -23,8 +22,7 @@
 			
 			//Define Default option using extend 
             var options = $.extend(defaults, options);
-			
-			var fileinput = $(this).find('input[type=file]').css({"width":"100%", "height":"100%", "position":"absolute", "left":"0", "top":"0" });
+			var fileinput = $(this).css({"width":"100%", "height":"100%", "position":"absolute", "left":"0", "top":"0" });
 			fileinput.addClass(options.fileinputclass); //adding Class in to File input
 			fileinput.css({opacity:0}); // Hide File input
 			
@@ -32,6 +30,7 @@
 			var customfile = $('<div class="'+options.customboxclass+'"><div class="innersec"></div></div>').css({"position":"relative"}); //custome file wrapper
 			var cfilefield = $('<span class="cust-field"></span>'); //custome file field
 			var cfilebutton = $('<span class="cust-btn"></span>').text(options.buttontext); //custome file button
+			
 			
 			//Adding file name In Custome design
 			fileinput.bind('change', function(){
@@ -52,6 +51,7 @@
 				var leftpos =  e.pageX - offleft - 190;
 				var toppos =  e.pageY - offtop-5 ;
 			});
+			
 			$("."+movebox).bind('mouseleave', function(e){
 				$(this).find('input[type=file]').css({left: 0, top: 0});
 			}).width(options.customboxwidth);
